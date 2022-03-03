727478a9f836caef26df607d0955c016a3a0cf60
tatsuya.sakamoto <tatsuya.sakamoto@JMPC-154.local>
2022-03-04 07:35:12 +0900

    ToolBarにメニュー表示

--- app/src/main/java/com/example/toolbarsample2/MainActivity.kt
+++ app/src/main/java/com/example/toolbarsample2/MainActivity.kt
@@ -2,11 +2,36 @@ package com.example.toolbarsample2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.core.view.MenuItemCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.my_toolbar))
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.action_setting -> {
+                return true
+            }
+            R.id.action_favorite -> {
+                return true
+            }
+            else -> {
+                super.onOptionsItemSelected(item)
+            }
+        }
+        return true
     }
 }
